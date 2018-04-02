b8adb877c3baf2674274ab717ccd4c80e1a98f1a
ChenHao <b03505028@ntu.edu.tw>
2018-04-02 13:57:49 +0800

    fft gogo

--- hw2/hw2_ST/FAS.v
+++ hw2/hw2_ST/FAS.v
@@ -79,7 +79,39 @@ module FIR_FILTER (clk, rst, data_valid, data, fir_valid, fir_d);
   output fir_valid;
   output signed [15:0] fir_d;
 
-  `include "./dat/FIR_coefficient.dat"
+  /* ============================================ */
+  parameter signed [19:0] FIR_C00 = 20'hFFF9E ;  //The FIR_coefficient value  0: -1.495361e-003
+  parameter signed [19:0] FIR_C01 = 20'hFFF86 ;  //The FIR_coefficient value  1: -1.861572e-003
+  parameter signed [19:0] FIR_C02 = 20'hFFFA7 ;  //The FIR_coefficient value  2: -1.358032e-003
+  parameter signed [19:0] FIR_C03 = 20'h0003B ;  //The FIR_coefficient value  3:  9.002686e-004
+  parameter signed [19:0] FIR_C04 = 20'h0014B ;  //The FIR_coefficient value  4:  5.050659e-003
+  parameter signed [19:0] FIR_C05 = 20'h0024A ;  //The FIR_coefficient value  5:  8.941650e-003
+  parameter signed [19:0] FIR_C06 = 20'h00222 ;  //The FIR_coefficient value  6:  8.331299e-003
+  parameter signed [19:0] FIR_C07 = 20'hFFFE4 ;  //The FIR_coefficient value  7: -4.272461e-004
+  parameter signed [19:0] FIR_C08 = 20'hFFBC5 ;  //The FIR_coefficient value  8: -1.652527e-002
+  parameter signed [19:0] FIR_C09 = 20'hFF7CA ;  //The FIR_coefficient value  9: -3.207397e-002
+  parameter signed [19:0] FIR_C10 = 20'hFF74E ;  //The FIR_coefficient value 10: -3.396606e-002
+  parameter signed [19:0] FIR_C11 = 20'hFFD74 ;  //The FIR_coefficient value 11: -9.948730e-003
+  parameter signed [19:0] FIR_C12 = 20'h00B1A ;  //The FIR_coefficient value 12:  4.336548e-002
+  parameter signed [19:0] FIR_C13 = 20'h01DAC ;  //The FIR_coefficient value 13:  1.159058e-001
+  parameter signed [19:0] FIR_C14 = 20'h02F9E ;  //The FIR_coefficient value 14:  1.860046e-001
+  parameter signed [19:0] FIR_C15 = 20'h03AA9 ;  //The FIR_coefficient value 15:  2.291412e-001
+  parameter signed [19:0] FIR_C16 = 20'h03AA9 ;  //The FIR_coefficient value 16:  2.291412e-001
+  parameter signed [19:0] FIR_C17 = 20'h02F9E ;  //The FIR_coefficient value 17:  1.860046e-001
+  parameter signed [19:0] FIR_C18 = 20'h01DAC ;  //The FIR_coefficient value 18:  1.159058e-001
+  parameter signed [19:0] FIR_C19 = 20'h00B1A ;  //The FIR_coefficient value 19:  4.336548e-002
+  parameter signed [19:0] FIR_C20 = 20'hFFD74 ;  //The FIR_coefficient value 20: -9.948730e-003
+  parameter signed [19:0] FIR_C21 = 20'hFF74E ;  //The FIR_coefficient value 21: -3.396606e-002
+  parameter signed [19:0] FIR_C22 = 20'hFF7CA ;  //The FIR_coefficient value 22: -3.207397e-002
+  parameter signed [19:0] FIR_C23 = 20'hFFBC5 ;  //The FIR_coefficient value 23: -1.652527e-002
+  parameter signed [19:0] FIR_C24 = 20'hFFFE4 ;  //The FIR_coefficient value 24: -4.272461e-004
+  parameter signed [19:0] FIR_C25 = 20'h00222 ;  //The FIR_coefficient value 25:  8.331299e-003
+  parameter signed [19:0] FIR_C26 = 20'h0024A ;  //The FIR_coefficient value 26:  8.941650e-003
+  parameter signed [19:0] FIR_C27 = 20'h0014B ;  //The FIR_coefficient value 27:  5.050659e-003
+  parameter signed [19:0] FIR_C28 = 20'h0003B ;  //The FIR_coefficient value 28:  9.002686e-004
+  parameter signed [19:0] FIR_C29 = 20'hFFFA7 ;  //The FIR_coefficient value 29: -1.358032e-003
+  parameter signed [19:0] FIR_C30 = 20'hFFF86 ;  //The FIR_coefficient value 30: -1.861572e-003
+  parameter signed [19:0] FIR_C31 = 20'hFFF9E ;  //The FIR_coefficient value 31: -1.495361e-003
 
   /* ============================================ */
   reg [5:0] fir_cnt_r, fir_cnt_w;
@@ -171,38 +203,37 @@ module FIR_FILTER (clk, rst, data_valid, data, fir_valid, fir_d);
       x_01_w = x_02_r;
       x_00_w = x_01_r;
 
-      sum_w = 0
-            + x_31_r * FIR_C00 
-            + x_30_r * FIR_C01 
-            + x_29_r * FIR_C02 
-            + x_28_r * FIR_C03 
-            + x_27_r * FIR_C04 
-            + x_26_r * FIR_C05 
-            + x_25_r * FIR_C06 
-            + x_24_r * FIR_C07 
-            + x_23_r * FIR_C08 
-            + x_22_r * FIR_C09 
-            + x_21_r * FIR_C10 
-            + x_20_r * FIR_C11 
-            + x_19_r * FIR_C12 
-            + x_18_r * FIR_C13 
-            + x_17_r * FIR_C14 
-            + x_16_r * FIR_C15 
-            + x_15_r * FIR_C16 
-            + x_14_r * FIR_C17 
-            + x_13_r * FIR_C18 
-            + x_12_r * FIR_C19 
-            + x_11_r * FIR_C20 
-            + x_10_r * FIR_C21 
-            + x_09_r * FIR_C22 
-            + x_08_r * FIR_C23 
-            + x_07_r * FIR_C24 
-            + x_06_r * FIR_C25 
-            + x_05_r * FIR_C26 
-            + x_04_r * FIR_C27 
-            + x_03_r * FIR_C28 
-            + x_02_r * FIR_C29 
-            + x_01_r * FIR_C30 
+      sum_w = x_31_r * FIR_C00
+            + x_30_r * FIR_C01
+            + x_29_r * FIR_C02
+            + x_28_r * FIR_C03
+            + x_27_r * FIR_C04
+            + x_26_r * FIR_C05
+            + x_25_r * FIR_C06
+            + x_24_r * FIR_C07
+            + x_23_r * FIR_C08
+            + x_22_r * FIR_C09
+            + x_21_r * FIR_C10
+            + x_20_r * FIR_C11
+            + x_19_r * FIR_C12
+            + x_18_r * FIR_C13
+            + x_17_r * FIR_C14
+            + x_16_r * FIR_C15
+            + x_15_r * FIR_C16
+            + x_14_r * FIR_C17
+            + x_13_r * FIR_C18
+            + x_12_r * FIR_C19
+            + x_11_r * FIR_C20
+            + x_10_r * FIR_C21
+            + x_09_r * FIR_C22
+            + x_08_r * FIR_C23
+            + x_07_r * FIR_C24
+            + x_06_r * FIR_C25
+            + x_05_r * FIR_C26
+            + x_04_r * FIR_C27
+            + x_03_r * FIR_C28
+            + x_02_r * FIR_C29
+            + x_01_r * FIR_C30
             + x_00_r * FIR_C31;
     end else begin
       sum_w     = 0;
