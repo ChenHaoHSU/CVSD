d48049d18ea37e57410e53386bca2726db3fb9cc
ChenHao <b03505028@ntu.edu.tw>
2018-04-30 15:10:43 +0800

    lulu

--- hw3/HW3_ST/LMFE.v
+++ hw3/HW3_ST/LMFE.v
@@ -1,50 +1,58 @@
 /****************************************************************
   Top Module
 *****************************************************************/
-module LMFE ( clk, reset, Din, in_en, busy, out_valid, Dout );
-input   clk;
-input   reset;
-input   in_en;
-output  busy;
-output  out_valid;
-input   [7:0]  Din;
-output  [7:0]  Dout;
+module lmfe_top (
+	clk,
+	reset,
+	Din,
+	in_en,
+	busy,
+	out_valid,
+	Dout
+);
 
-/* ============================================ */
+//-- I/O declaration
+input			clk;
+input			reset;
+input	[7:0]	Din;
+input			in_en;
+output			busy;
+output			out_valid;
+output	[7:0]	Dout;
+
+//-- reg and wire
+wire	[9:0]	sram_address;
+wire	[7:0]	sram_in;
+wire	[7:0]	sram_out;
 wire	[7:0]	sort_insert;
 wire	[7:0]	sort_delete;
 wire	[7:0]	sort_median;
-wire		   	sort_enable;
+wire 			chip_enable;
+wire			write_enable;
+wire			sort_enable;
 
-wire 		   	sram_cen;
-wire		   	sram_wen;
-wire	[9:0]	sram_addr;
-wire	[7:0]	sram_d;
-wire	[7:0]	sram_q;
-
-/* ============================================ */
-controller lmfe_controller (
-  // input
-  .CLK	(clk),
-  .RST	(reset),
-  .IEN  (in_en),
-  .DIN	(Din),
-  .Q		(sram_q),
-  .MED	(sort_median),
-  // output
-  .ADDR	(sram_addr),
-  .D		(sram_d),	
-  .CEN	(sram_cen),
-  .WEN	(sram_wen),
-  .SEN	(sort_enable),
-  .INS	(sort_insert),
-  .DEL	(sort_delete),
-  .DOUT	(Dout),
-  .OV		(out_valid),
-  .BUSY	(busy)
+lmfe_filter_ctrl i_lmfe_filter_ctrl (
+	//-- input port
+	.clk	(clk),
+	.RST	(reset),
+	.IEN	(in_en),
+	.DIN	(Din),
+	.Q		(sram_out),
+	.MED	(sort_median),
+	//-- output port
+	.A		(sram_address),
+	.D		(sram_in),	
+	.CE		(chip_enable),
+	.WE		(write_enable),
+	.SE		(sort_enable),
+	.INS	(sort_insert),
+	.DEL	(sort_delete),
+	.DOUT	(Dout),
+	.OV		(out_valid),
+	.BZ		(busy)
 );
 
-med49 lmfe_med49 (
+lmfe_med49 i_lmfe_med49 (
 	//-- input port
 	.clk	(clk),
 	.RST	(reset),
@@ -55,15 +63,15 @@ med49 lmfe_med49 (
 	.MED	(sort_median)
 );
 
-sram_1024x8_t13 lmfe_sram (
-  // input
-  .CLK	(clk),
-  .CEN	(sram_cen),
-  .WEN	(sram_wen),
-  .A    (sram_addr),
-  .D    (sram_d),
-  // output
-  .Q    (sram_q)
+sram_1024x8_t13 i_ram0 (
+	//-- input port
+	.CLK	(clk),
+	.CEN	(chip_enable),
+	.WEN	(write_enable),
+	.A		(sram_address),
+	.D		(sram_in),
+	//-- output port
+	.Q		(sram_out)
 );
 
 endmodule
