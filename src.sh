884de24dff6b24b40550b395a8add664f90b084a
ChenHaoHSU <b03505028@ntu.edu.tw>
2018-05-13 18:22:13 +0800

    upd lab3

--- src.sh
+++ src.sh
@@ -1,4 +1,5 @@
 source /usr/cadence/CIC/incisiv.cshrc
-source /usr/spring_soft/CIC/verdi.cshrc
 source /usr/cadence/cshrc
+source /usr/spring_soft/CIC/verdi.cshrc
 source /usr/cad/synopsys/CIC/primetime.cshrc
+source /usr/cad/synopsys/CIC/synthesis.cshrc
